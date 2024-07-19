75ab501498ced2dbf26d1f2e826de91157ee7c67
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-19 08:54:43 +0530

    training function created

--- moe/torch_moe/expert.py
+++ moe/torch_moe/expert.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch.optim as optim
 
 class Expert(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -12,6 +12,16 @@ class Expert(nn.Module):
     def forward(self, x):
         x = torch.relu(self.layer1(x))
         return torch.softmax(self.layer2(x), dim=1)
+    
+    def train_expert(self, x_train, y_train, epochs=500, learning_rate=0.001):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            outputs = self(x_train)
+            loss = criterion(outputs, y_train)
+            loss.backward()
+            optimizer.step()
     
 
     def save_model(self, path:str):
